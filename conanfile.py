e3df06b2854fefeca60d68a0ff30757760f72a20
Björn Stresing <bjoern.stresing@gmx.de>
2024-06-16 19:27:22 +0200

    Fix Linux CI

--- conanfile.py
+++ conanfile.py
@@ -52,6 +52,12 @@ class QtIFConan(ConanFile):
         elif self.settings.os == "Windows":
             return self.installer_name
 
+    def system_requirements(self):
+        if self.settings.os == "Linux":
+            apt = Apt(self)
+            pack_names = ["libgl1-mesa-dev", "libfontconfig1-dev", "libfreetype6-dev", "libx11-dev", "libx11-xcb-dev", "libxext-dev", "libxfixes-dev", "libxi-dev", "libxrender-dev", "libxcb1-dev", "libxcb-cursor-dev", "libxcb-glx0-dev", "libxcb-keysyms1-dev", "libxcb-image0-dev", "libxcb-shm0-dev", "libxcb-icccm4-dev", "libxcb-sync-dev", "libxcb-xfixes0-dev", "libxcb-shape0-dev", "libxcb-randr0-dev", "libxcb-render-util0-dev", "libxcb-util-dev", "libxcb-xinerama0-dev", "libxcb-xkb-dev", "libxkbcommon-dev", "libxkbcommon-x11-dev"]
+            apt.install(pack_names, update=True)
+
     def validate(self):
         valid_os = ["Windows", "Linux"]
         if str(self.settings.os) not in valid_os:
